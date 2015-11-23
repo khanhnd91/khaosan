28d12da668855f04948800f21ac6c525c461e697
codelovers <codelove@CodeLoves-Mac-mini.local>
2015-11-23 18:10:20 +0700

    hotel sidebar

---
+++ wp-content/themes/khaosan/hotel-sidebar.php
@@ -0,0 +1,26 @@
+        <nav class="hostel">
+            <!-- menu area -->
+            <div class="menu_area">
+            <div class="arrow" style="top: 92px; left: 12px;">
+                <img src="img/menu/hostel/arrow.png">
+            </div>
+                <ul>
+                    <li><img src="img/menu/hostel/top.png">Top</li>
+                    <li><img src="img/menu/hostel/facilities.png">RoomType / Rates</li>
+                    <li><img src="img/menu/hostel/access.png">Access</li>
+                </ul>
+            </div>
+            <!-- information -->
+            <div class="information">
+                <p>3-16-10, NishiAsakusa,Taito-ku,Tokyo
+                Postcode:111-0035</p>
+                <p>+81-3-3844-0011</p>
+                <p>mail: <a href="mailto:samurai@khaosan-tokyo.com">samurai@khaosan-tokyo.com</a></p>
+                <p>
+                *online reservation only<br>
+                *reception business hour 8am - 9pm<br>
+                *check-in: 3pm - 9pm<br>
+                *check-out: by 11am<br>
+                </p>
+            </div>
+        </nav>
